184855c9612b371172da3a59ee4ca4de191e65b2
LIU YIYANG : ) <haixiang1977@users.noreply.github.com>
2022-06-18 22:16:40 +0800

    update the code and pass the test

--- test/randomdata.sol
+++ test/randomdata.sol
@@ -5,6 +5,11 @@ import '@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol';
 import '@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol';
 
 contract ZhiShaiZi is VRFConsumerBaseV2 {
+    // when callback not called, the state will be ROLL_IN_PROGRESS
+    uint256 private constant ROLL_END = 1000;
+    uint256 private constant ROLL_IN_PROGRESS = 100;
+    uint256 rollState = ROLL_END;
+
     VRFCoordinatorV2Interface COORDINATOR;
 
     // chainlink subscription id
@@ -13,9 +18,11 @@ contract ZhiShaiZi is VRFConsumerBaseV2 {
     // chainlink configure
     address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
     bytes32 s_keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
-    uint32 callbackGasLimit = 40000;
+    uint32 callbackGasLimit = 100000;
     uint16 requestConfirmations = 3;
     uint32 numWords = 1;
+
+    uint256 diceValue = 20;
 
     event DiceRolled(uint256 indexed requestId, address indexed roller);
     event DiceLanded(uint256 indexed requestId, uint256 indexed result);
@@ -35,6 +42,8 @@ contract ZhiShaiZi is VRFConsumerBaseV2 {
             numWords
         );
 
+        rollState = ROLL_IN_PROGRESS;
+
         emit DiceRolled(requestId, roller);
     }
 
@@ -42,6 +51,15 @@ contract ZhiShaiZi is VRFConsumerBaseV2 {
     // % 16 = [0, 15] then +3 reshape to [3, 18]
     function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
         uint256 d20Value = (randomWords[0] % 16) + 3;
+        diceValue = d20Value;
+
+        rollState = ROLL_END;
         emit DiceLanded(requestId, d20Value);
+    }
+
+    function getValue() public view returns (uint256 val) {
+        require(rollState != ROLL_IN_PROGRESS, "Roll in progress");
+        require(diceValue != 20, "Roll not started");
+        return diceValue;
     }
 }
