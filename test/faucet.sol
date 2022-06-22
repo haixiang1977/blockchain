db08de66cfd1d5c9c925f4a322472977dd7d30c5
LIU YIYANG : ) <haixiang1977@users.noreply.github.com>
2022-06-22 22:23:41 +0800

    Create faucet.sol

---
+++ test/faucet.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+// transfer money to smart contract
+pragma solidity ^0.8.7;
+
+contract faucet {
+    address public owner;
+
+    // need to set value to 1 Finney as initial balance
+    // then can withdraw from this account
+
+    constructor () payable {
+        // owner is the person who currently connect to the contract
+        owner = msg.sender;
+    }
+
+    function withdraw(uint amount) public payable {
+        require (address(this).balance > amount);
+        // need to mark as payable
+        payable(msg.sender).transfer(amount);
+    }
+
+    function getbalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
