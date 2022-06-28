dc491b8952c9e887ca894ac1e4f157ef8cc03247
LIU YIYANG : ) <haixiang1977@users.noreply.github.com>
2022-06-28 21:29:55 +0800

    Create index.js

---
+++ test/web3js/test3/index.js
@@ -0,0 +1,19 @@
+// display a customized html page
+
+var express = require('express');
+var fs = require('fs');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+  fs.readFile('../public/index.html', function(err, html) {
+    if (err) {
+      throw err;
+    }
+    res.write(html);
+    res.end();
+  });
+});
+
+module.exports = router;
